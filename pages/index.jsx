1a472915753a8056f8af16eb87c5f9cb2a125b61
sadness_ojisan <sadness.ojisan@gmail.com>
2020-07-02 04:22:56 +0900

    success handle router

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,32 @@
 import React, { useState } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default () => {
   const [ids] = useState([1, 2, 3, 4, 5, 6, 7, 8]);
+  const router = useRouter();
   return (
     <div>
+      <h2>linkでjump</h2>
       {ids.map((id) => {
         return (
           <div>
             <Link href={`cards#${id}`}>
               <a>{id}へJUMP</a>
             </Link>
+          </div>
+        );
+      })}
+      <hr></hr>
+      <h2>linkを使わずにrouterでjump</h2>
+      {ids.map((id) => {
+        return (
+          <div
+            onClick={() => {
+              router.push(`/cards#${id}`);
+            }}
+          >
+            {id}へJUMP
           </div>
         );
       })}
